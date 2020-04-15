016934bae834ad41a33b8d3d30b756828f5b3c1d
Luigi Octaviano <luigioctaviano@gmail.com>
2020-04-15 23:17:49 +0800

    Basic Info - About Section

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,6 +8,41 @@
       </div>
     </div>
     <div class="about-container" id="about" v-show="show" v-observe-visibility="visibilityChanged">
+      <div class="header-content about-header">
+        <h1 class="title">About Me</h1>
+      </div>
+      <div class="about-content">
+        <br>
+        <p class="title"><span class="title-html">&lt;</span> <b>Basic</b> Information <span class="title-html">/&gt;</span></p>
+        <div class="basic-info">
+          <div class="pic-container">
+            <div class="pic-content"></div>
+          </div>
+          <div class="myself-content">
+            <p><strong>Age:</strong> 24 Years Old</p>
+            <p><strong>Years of Experience:</strong> 3+ years</p>
+            <p><strong>Educational Attainment:</strong> Bachelor's Degree</p>
+            <p><strong>Year Graduated:</strong> 2017</p>
+          </div>
+        </div>
+        <!-- <br>
+        <p class="title"><span class="title-html">&lt;</span> <b>My</b> Skills <span class="title-html">/&gt;</span></p>
+        <div class="skills-info">
+          <div class="columns skills-items">
+            <div class="column skills-item">
+              <h1 class="title">Item # 1</h1>
+            </div>
+            <div class="column skills-item">
+              <h1 class="title">Item # 2</h1>
+            </div>
+            <div class="column skills-item">
+              <h1 class="title">Item # 3</h1>
+            </div>
+          </div>
+        </div> -->
+      </div>
+    </div>
+    <div class="works-container" id="works">
       <Maintenance />
     </div>
   </div>
